4581dcab99c1f96fe39bb948f72b41fff0e73127
Steven Alden <88414755+stevena1304@users.noreply.github.com>
2023-06-28 22:12:05 +0700

    notif done

--- resources/views/notification/notif-fetch.blade.php
+++ resources/views/notification/notif-fetch.blade.php
@@ -1,6 +1,14 @@
+<a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+  <i class="bi bi-bell"></i>
+  @if($notifications->count() > 0)
+  <span class="badge bg-primary badge-number">{{count($notifications)}}</span>
+  @endif
+</a>
+<!-- End Notification Icon -->
 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
   <li class="dropdown-header">
     You have {{count($notifications)}} new notifications
+    <p style="font-size: 11px">Click to dismiss</p>
     <!-- <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a> -->
   </li>
   @if($notifications->count() > 0)
